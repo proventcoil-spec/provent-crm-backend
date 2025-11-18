3df3ab625a55f9b0742ec10a6b6aec2fa1280680
proventcoil-spec <provent.co.il@gmail.com>
2025-11-18 22:46:02 +0200

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,14 +1,13 @@
-// src/db.js
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST,      // srv1016.hstgr.io
+  user: process.env.DB_USER,      // u894002499_provi  (אצלך)
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME,  // u894002499_provi_crm
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
