f7ea7a0db0a75e965e0c597ca5627505e62a807c
proventcoil-spec <provent.co.il@gmail.com>
2025-11-25 16:05:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-# app.py
 from flask import Flask
-
+from flask_cors import CORS
+from config import Config
 from extensions import init_db
 from auth_routes import auth_bp
 from clients_routes import clients_bp
@@ -8,14 +8,14 @@ from events_routes import events_bp
 from leads_routes import leads_bp
 from uploads_routes import uploads_bp
 
-
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    # DB + CORS
+    CORS(app, supports_credentials=True)
+
     init_db(app)
 
-    # רישום ראוטים
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(clients_bp, url_prefix="/api/clients")
     app.register_blueprint(events_bp, url_prefix="/api/events")
@@ -24,8 +24,6 @@ def create_app():
 
     return app
 
-
-# עבור gunicorn ברנדר
 app = create_app()
 
 if __name__ == "__main__":
