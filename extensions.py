2bb9c413fe438f1e928c29d831ccb030f6e70f05
proventcoil-spec <provent.co.il@gmail.com>
2025-11-25 16:17:45 +0200

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI']
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
